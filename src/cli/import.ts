8466aa59a4d1890beb539cc1a96c8d262e7c1f8e
Trinketer22 <trinketer22@localhost>
2024-09-01 23:46:20 +0300

    Import progress bar

--- src/cli/import.ts
+++ src/cli/import.ts
@@ -3,6 +3,7 @@ import sqlite3 from 'sqlite3';
 import arg from 'arg';
 import {open, FileHandle} from 'node:fs/promises';
 import { Address } from '@ton/core';
+import cliProgress from 'cli-progress';
 
 let importFile: FileHandle;
 let db: sqlite3.Database;
@@ -28,11 +29,14 @@ async function create_table() {
 
 function help() {
     console.log("Import airdrop csv data into database");
+    console.log("--count [number of records] set record count without reading the file");
     console.log(`${__filename} <csv_path> <database_path>`);
 }
 
 async function run() {
-    const args = arg({});
+    const args = arg({
+        '--count': Number
+    });
 
     if(args._.length == 0) {
         console.log("Path to import csv is required");
@@ -53,24 +57,52 @@ async function run() {
 
     let insertParams: string[] = [];
     let placeholders: string[] = [];
-    let recCount = 0;
+    let recCount: number;
+    let processed = 0;
+    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+    if(args['--count'] === undefined) {
+        console.log("Counting records...");
+        recCount = 0;
+        for await (const line of importFile.readLines({encoding: 'utf8'})) {
+            recCount++;
+        }
+    }
+    else {
+        recCount = args['--count'];
+        if(recCount < 0) {
+            console.log("Record count should be > 0");
+            return;
+        }
+    }
+
+    console.log("Rec count:", recCount);
+
+    await importFile.close();
+    importFile = await open(args._[0], 'r');
+    bar.start(recCount, processed)
+
     for await (const line of importFile.readLines({encoding: 'utf8'})) {
         const [addr, amount] = line.split(',');
         const parsedAddr     = Address.parse(addr);
         insertParams.push(...[parsedAddr.toRawString(), amount.toString(), Number('0x' + parsedAddr.hash.subarray(0, 4).toString('hex')).toString() ]);
         placeholders.push('(?, ?, ?)');
-        if(++recCount % 10000 == 0) {
+        if(++processed % 10000 == 0) {
             db.run("INSERT INTO `airdrop` (`address`, `amount`, `key`) VALUES " + placeholders.join(','), insertParams);
             insertParams = [];
             placeholders = [];
+            bar.update(processed);
         }
     }
     if(insertParams.length > 0) {
         // console.log("Inserting last batch");
         db.run("INSERT INTO `airdrop` (`address`, `amount`, `key`) VALUES " + placeholders.join(','), insertParams);
+        bar.update(processed + insertParams.length);
     }
+    bar.stop();
+    console.log("Creating address index...");
     db.run("CREATE UNIQUE INDEX `addr_idx` ON `airdrop` (`address`)");
     db.close();
+    console.log("Done!");
 }
 
 run();
